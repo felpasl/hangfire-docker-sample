c6c59ec5cf64aa8544fed3a5f1957ae2b2c68284
Felipe Lambert <felipe.lambert@outlook.com>
2019-03-06 16:05:30 -0300

    intial commit

---
+++ Hangfire.Docker.Web/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Hangfire.PostgreSql;
+
+namespace Hangfire.Docker.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(config => config.UsePostgreSqlStorage(Environment.GetEnvironmentVariable("HANGFIREDB_CONN")));
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseHangfireDashboard("/hangfire",new DashboardOptions
+            {
+                Authorization = new[] { new MyAuthorizationFilter() }
+            });
+            app.UseMvc();
+        }
+    }
+}
---
+++ Hangfire.Docker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Hangfire;
+using Hangfire.PostgreSql;
+namespace Hangfire.Docker
+{
+    class Program
+    {
+        private static readonly AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+        static void Main(string[] args)
+        {
+
+            GlobalConfiguration.Configuration.UsePostgreSqlStorage(Environment.GetEnvironmentVariable("HANGFIREDB_CONN"));
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started.");
+
+                RecurringJob.AddOrUpdate(() => Run()
+                , Cron.Minutely);
+
+                AppDomain.CurrentDomain.ProcessExit += (o, e) =>
+                {
+                    server.Dispose();
+                    Console.WriteLine("Terminating...");
+                    autoResetEvent.Set();
+                };
+
+                autoResetEvent.WaitOne();
+            }
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine("Minute Job");
+        }
+    }
+}
